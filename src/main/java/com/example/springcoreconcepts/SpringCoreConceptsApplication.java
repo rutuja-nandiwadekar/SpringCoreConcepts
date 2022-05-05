56831d03f366839df7d31a49582d383ea0992d58
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-05 22:49:13 +0530

    Added: logger to the Log messages using SLF4J

--- src/main/java/com/example/springcoreconcepts/SpringCoreConceptsApplication.java
+++ src/main/java/com/example/springcoreconcepts/SpringCoreConceptsApplication.java
@@ -1,8 +1,9 @@
 package com.example.springcoreconcepts;
 
 
-
 import com.example.springcoreconcepts.components.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,11 +11,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringCoreConceptsApplication {
 
+	public static final Logger logger = (Logger) LoggerFactory.getLogger(SpringCoreConceptsApplication.class);
+
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept Demo");
+		logger.error("Welcome to Spring Concept Demo");
 		ApplicationContext context = SpringApplication.run(SpringCoreConceptsApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("Demo Bean = "+demoBean.toString());
-		System.out.println(context.getBean(HelloRestController.class));
+		logger.error("Demo Bean = "+demoBean.toString());
+		HelloRestController controller = context.getBean(HelloRestController.class);
+		logger.error("HelloRestController = "+controller.toString());
 	}
 }
